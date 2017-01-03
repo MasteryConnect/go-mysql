74b682cc618aae4351111ef757e0e38d1fa9e327
Doug Weber <doug@masteryconnect.com>
2017-01-03 14:39:40 +0000

    Add basic column count mismatch to trigger table schema reload.

--- canal/sync.go
+++ canal/sync.go
@@ -79,10 +79,7 @@ func (c *Canal) handleRowsEvent(e *replication.BinlogEvent) error {
 	ev := e.Event.(*replication.RowsEvent)
 
 	// Caveat: table may be altered at runtime.
-	schema := string(ev.Table.Schema)
-	table := string(ev.Table.Table)
-
-	t, err := c.GetTable(schema, table)
+	t, err := c.GetTableForEvent(ev.Table)
 	if err != nil {
 		return errors.Trace(err)
 	}
